7ac247ba9da90346829de618f128efcb60661fa6
SamuelKamal13 <samuelkamal131@gmail.com>
2024-12-22 01:13:46 +0200

    Delete patterns directory

--- patterns/adapters/BillingSystem.java
+++
@@ -1,9 +0,0 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package patterns.adapters;
-
-public interface BillingSystem {
-    void processPayment(String patientId, double amount);
-}
